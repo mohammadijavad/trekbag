23b5d3aed11d48dfb7c0e444df76b536c7bd3b9c
javad <javadmohammadi.js@gmail.com>
2024-09-12 03:05:51 -0600

    add button group list to handle some functionality

--- src/components/button-group.jsx
+++ src/components/button-group.jsx
@@ -1,3 +1,34 @@
+import Button from "./button";
+
 export default function ButtonGroup(){
-    return <button disabled> button</button>
+const secondaryButtons = [
+    {
+        text: "Mark all as complete",
+        onClick: () => console.log('i am work'),
+    },
+    {
+        text: "Mark all as incomplete",
+        onClick: () => console.log('i am work'),
+    },
+    {
+        text: "Reset to initial",
+        onClick: () => console.log('i am work'),
+    },
+    {
+        text: "Remove all items",
+        onClick: () => console.log('i am work'),
+    },
+];
+return (
+    <section className="button-group">
+        {secondaryButtons.map((button) => (
+            <Button
+                key={button.text + button.onClick.toString()}
+                text={button.text}
+                onClick={button.onClick}
+                buttonType="secondary"
+            />
+        ))}
+    </section>
+)
 }
