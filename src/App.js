4bea68006b301579931bf791977fb352e0fdf7c9
javad <javadmohammadi.js@gmail.com>
2024-09-12 02:45:31 -0600

    add header itemList and sidebar file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import BackgroundHeading from "./components/background-heading";
 import Footer from "./components/footer";
+import Header from "./components/header";
+import SideBar from "./components/side-bar";
+import ItemList from "./components/item-list";
 
 function App() {
   return (
     <>
     <BackgroundHeading/>
-      <main></main>
+      <main>
+        <Header/>
+        <ItemList/>
+        <SideBar/>
+      </main>
       <Footer/>
     </>
   );
---
+++ src/components/item-list.jsx
@@ -0,0 +1,5 @@
+export default function ItemList(){
+    return <div>
+        item list
+    </div>
+}
