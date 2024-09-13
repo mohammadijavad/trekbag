37fd63c413784120b83a5b8d68dfa1faaa07df84
javad <javadmohammadi.js@gmail.com>
2024-09-13 02:52:45 -0600

    add initialItems to show default items

--- src/components/item-list.jsx
+++ src/components/item-list.jsx
@@ -1,5 +1,26 @@
+import {useState} from "react";
+import {initialItems} from "../lib/constants";
+
 export default function ItemList(){
+    const [items,setItems]=useState(initialItems)
     return <div className='item-list'>
-        item list
+        {items.map((item)=><Item key={item.id} {...item} />)}
     </div>
+}
+
+
+function Item({packed=false,name=''}){
+    return (
+        <li className="item">
+            <label>
+                <input
+                    checked={packed}
+                    type="checkbox"
+                />{" "}
+                {name}
+            </label>
+
+            <button >❌</button>
+        </li>
+    )
 }
