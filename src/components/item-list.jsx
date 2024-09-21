48dc59bd2ee949cd20224a66fe4f2e30fcc3da3d
javad <javadmohammadi.js@gmail.com>
2024-09-21 03:20:58 -0600

    add react select functionality to sort items depend on packed

--- src/components/item-list.jsx
+++ src/components/item-list.jsx
@@ -1,15 +1,58 @@
 import EmptyView from "./empty-view";
+import { useMemo, useState } from "react";
+import Select from "react-select";
 
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default",
+  },
+  {
+    label: "Sort by packed",
+    value: "packed",
+  },
+  {
+    label: "Sort by unpacked",
+    value: "unpacked",
+  },
+];
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
+  const [sortBy, setSortBy] = useState("default");
+
+  const sortedItems = useMemo(
+    () =>
+      [...items].sort((a, b) => {
+        if (sortBy === "packed") {
+          return b.packed - a.packed;
+        }
+
+        if (sortBy === "unpacked") {
+          return a.packed - b.packed;
+        }
+
+        return;
+      }),
+    [items, sortBy],
+  );
+
   return (
     <ul className="item-list">
       {items.length === 0 ? <EmptyView /> : null}
-      {items.length > 0 &&
-        items.map((item) => (
+      {items.length > 0 ? (
+        <div className="sorting">
+          <Select
+            defaultValue={sortingOptions[0].value}
+            onChange={(option) => setSortBy(option.value)}
+            options={sortingOptions}
+          />
+        </div>
+      ) : null}
+      {sortedItems.length > 0 &&
+        sortedItems.map((item) => (
           <Item
             key={item.id}
             {...item}
